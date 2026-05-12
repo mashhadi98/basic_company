08865fd0cdbea664f4523f0549c76b717e79702e
ali <ali@DESKTOP-VV19P79>
2026-05-12 13:20:48 +0330

    fix: fix active menu

---
+++ Company.Presentation/Extensions/HtmlExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+public static class HtmlExtensions
+{
+    public static string IsActive(this IHtmlHelper html, string controller, string action = null)
+    {
+        var currentController = html.ViewContext.RouteData.Values["controller"]?.ToString();
+        var currentAction = html.ViewContext.RouteData.Values["action"]?.ToString();
+
+        if (string.IsNullOrEmpty(action))
+            return currentController == controller ? "active" : "";
+
+        // برای Edit و Details هم منوی لیست فعال شود
+        if (action.Equals("Edit", StringComparison.OrdinalIgnoreCase) ||
+            action.Equals("Details", StringComparison.OrdinalIgnoreCase))
+        {
+            return currentController == controller ? "active" : "";
+        }
+
+        return currentController == controller && currentAction == action ? "active" : "";
+    }
+
+    public static string IsMenuOpen(this IHtmlHelper html, string controller)
+    {
+        var currentController = html.ViewContext.RouteData.Values["controller"]?.ToString();
+        return currentController == controller ? "menu-open" : "";
+    }
+}
